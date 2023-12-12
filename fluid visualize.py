f4c5e585d93c8ca13bf6471a7b055d256df1a7e3
pankratovds <151134009+pankratovds@users.noreply.github.com>
2023-12-12 23:04:57 +0300

    Add files via upload

---
+++ fluid visualize.py
@@ -0,0 +1,37 @@
+import pygame as pg
+pg.init()
+pg.draw.rect(50, 50, 100, 100)
+def visualize(event):
+    if event.button== self.mouse_button:
+       if self.rect.contains(event.pos, 1, 1):
+           main()
+for event in pg.event.get():
+    if event.type==pg.MOUSEBUTTONDOWN:
+        visualize()
+class Rect:
+    default_color = "brown"
+    def __init__(self, width, height):
+        self.width = 50
+        self.height = 50
+        
+class Text:  
+
+sc = pygame.display.set_mode((300,200))
+sc.fill((255, 255, 255))
+
+f1 = pygame.font.Font(None, 36)
+text1 = f1.render('Модель: квадрат', 1, (180, 0, 0))
+
+
+sc.blit(text1, (10, 50))
+
+
+pygame.display.update()
+
+while 1:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            exit()
+
+        
+        
